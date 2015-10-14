dd6e93a487fde7941438763b06910c95523b7047
gwjonker <gwjonker@gmail.com>
2015-10-14 12:02:35 +0200

    Starting Chrome and Firefox
    Starting WebDriver for browsers

---
+++ src/StartChrome.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;	
+
+
+public class StartChrome {
+	public static void main(String[] args) {
+		// chromedriver (in fact server) must be downloaded and placed anywhere
+		// also directory of chromedriver added to PATH-environment variable
+		// https://sites.google.com/a/chromium.org/chromedriver/getting-started 
+				
+		System.setProperty("webdriver.chrome.driver", "c:/devenv/chromeserver/chromedriver.exe");
+		WebDriver driver 		= new ChromeDriver();
+		
+		String appUrl 			= "https://accounts.google.com";
+	
+		driver.get(appUrl);
+		driver.manage().window().maximize();
+		
+		String actualTitle	= driver.getTitle();
+		System.out.println("Actual:   |" + actualTitle + "|");
+						
+		//driver.close();
+		System.exit(0);
+	}
+	
+	
+}
+
+	
+
---
+++ src/StartFirefox.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class StartFirefox {
+	public static void main(String[] args) {
+		WebDriver driver 		= new FirefoxDriver();
+		
+		String appUrl 			= "https://accounts.google.com";
+			
+		driver.get(appUrl);
+		
+		driver.manage().window().maximize();
+		
+		String actualTitle	= driver.getTitle();
+		System.out.println("Actual:   |" + actualTitle + "|");
+		
+		//driver.close();
+		System.exit(0);
+	}
+	
+	
+}
+
+	
+
